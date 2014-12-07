7f15c75df02c7783d42a5c827f424abbbb739984
manuelymotita <manuel@Mannys-MacBook-Pro.local>
2014-12-07 14:47:07 -0500

    !!!!!!!!

--- app.js
+++ app.js
@@ -101,20 +101,20 @@ $(document).ready(function(){
 			//portrait
 			$('#d1').text(Math.round(week[0].temperatureMin));
 			//$('#d1').text(week[0].summary);
-			$('#day2').text(Math.round(week[1].temperatureMin));
-			$('#d2').text(week[1].summary);
-			$('#day3').text(Math.round(week[2].temperatureMin));
-			$('#d3').text(week[2].summary);
-			$('#day4').text(Math.round(week[3].temperatureMin));
-			$('#d4').text(week[3].summary);
-			$('#day5').text(Math.round(week[4].temperatureMin));
-			$('#d5').text(week[4].summary);
-			$('#day6').text(Math.round(week[5].temperatureMin));
-			$('#d6').text(week[5].summary);
-			$('#day7').text(Math.round(week[6].temperatureMin));
-			$('#d7').text(week[6].summary);
-			$('#day8').text(Math.round(week[7].temperatureMin));
-			$('#d8').text(week[7].summary);
+			$('#d2').text(Math.round(week[1].temperatureMin));
+			//$('#d2').text(week[1].summary);
+			$('#d3').text(Math.round(week[2].temperatureMin));
+			//$('#d3').text(week[2].summary);
+			$('#d4').text(Math.round(week[3].temperatureMin));
+			//$('#d4').text(week[3].summary);
+			$('#d5').text(Math.round(week[4].temperatureMin));
+			//$('#d5').text(week[4].summary);
+			$('#d6').text(Math.round(week[5].temperatureMin));
+			//$('#d6').text(week[5].summary);
+			$('#d7').text(Math.round(week[6].temperatureMin));
+			//$('#d7').text(week[6].summary);
+			$('#d8').text(Math.round(week[7].temperatureMin));
+			//$('#d8').text(week[7].summary);
 			
 			// or add them one by one
 			var image = parseIcon(week[0].icon);
